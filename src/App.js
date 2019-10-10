9f24116123ecf292b86557cad8ffc03f07f6e3c6
Greg Hayes <gthelight@gmail.com>
2019-10-10 18:14:36 -0500

    Initial commit, api call working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
+import apod from "./components/apod";
 
 function App() {
+  const [photoData, setPhotoData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=5OJfH4G46HOISrf0mJx8LZs6h3XrtOUJR7g943u2`)
+      .then(response => {
+        console.log(response);
+        setPhotoData(response.data);
+      })
+      .catch(error => {
+        console.log('The data was not returned', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <p>
