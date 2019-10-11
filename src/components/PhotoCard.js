c68c6a15d046c1a352cf19bf3e0f36694113cb84
Greg Hayes <gthelight@gmail.com>
2019-10-11 09:57:11 -0500

    Added styled component

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,11 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const ColorChange = styled.p`
+  &:hover {
+    color: red;
+    transform: translateY(-5px) scale(1.5);
+  }
+`;
 
 const PhotoCard = props => {
   return (
     <div className="photo-data" key={props.id}>
       <h2>{props.title}</h2>
       <img src={props.imgUrl} />
-      <p>{props.date}</p>
+      <ColorChange className="color">{props.date}</ColorChange>
       <p>{props.description}</p>
       <p>{props.photographer}</p>
     </div>
